906c36dd5003af415a88da83c3fab315bdf006d3
Odie <93814478+odieQA-SDET@users.noreply.github.com>
2025-01-29 23:39:59 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,23 @@
 const { defineConfig } = require("cypress");
-
+const cypress = require("cypress");
 module.exports = defineConfig({
   e2e: {
-    baseUrl:"https://cmms.dev.aegislabs.work/#/page/assets/meters-&-groups",
+    watchForFileChanges:false,
+    chromeWebSecurity: false,
+    reporter: 'mocha-allure-reporter',
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+          require('cypress-mochawesome-reporter/plugin')(on),
+          on('task', {downloadFile}),
+          allureWriter(on, config);
+          allureCypress(on, config, {
+          resultsDir: "allure-results",
+      });
+          return config;    
+    },
+        include: [".node_modules/cypress", 'cypress/**/*.js'],
+    baseUrl: 'https://cmms.dev.aegislabs.work/#/page/assets/meters-&-groups'
     },
   },
 });
